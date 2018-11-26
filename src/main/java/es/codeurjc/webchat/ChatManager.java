788c3e0e3cb52f1dab95d143c3d0087f25f6c7bb
jramed <jramed@gmail.com>
2018-11-26 20:45:26 +0100

    Add some thread-safe code to Chatmanager.java (ongoing)
    Just first step. Add some comments as possible guidelines, anyway
    check them, don't follow them blindly.

--- src/main/java/es/codeurjc/webchat/ChatManager.java
+++ src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -2,15 +2,15 @@ package es.codeurjc.webchat;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class ChatManager {
 
-	private Map<String, Chat> chats = new HashMap<>();
-	private Map<String, User> users = new HashMap<>();
+	private ConcurrentMap<String, Chat> chats = new ConcurrentHashMap<>();
+	private ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
 	private int maxChats;
 
 	public ChatManager(int maxChats) {
@@ -19,27 +19,30 @@ public class ChatManager {
 
 	public void newUser(User user) {
 		
-		if(users.containsKey(user.getName())){
+		if(null != users.putIfAbsent(user.getName(), user)) {
 			throw new IllegalArgumentException("There is already a user with name \'"
 					+ user.getName() + "\'");
-		} else {
-			users.put(user.getName(), user);
 		}
 	}
 
 	public Chat newChat(String name, long timeout, TimeUnit unit) throws InterruptedException,
 			TimeoutException {
 
+		//TODO add mutual exclusion protection to ensure the size is not changed
+		//a new user could be been added while the checking is done	
 		if (chats.size() == maxChats) {
 			throw new TimeoutException("There is no enought capacity to create a new chat");
 		}
 
+		//TODO add mutal exclusion protection
 		if(chats.containsKey(name)){
 			return chats.get(name);
 		} else {
 			Chat newChat = new Chat(this, name);
+			//TODO add mutual exclusion protection
 			chats.put(name, newChat);
 			
+			//TODO should it be included in a mutual exclusion zone?
 			for(User user : users.values()){
 				user.newChat(newChat);
 			}
@@ -49,30 +52,36 @@ public class ChatManager {
 	}
 
 	public void closeChat(Chat chat) {
+		//TODO add mutual exclusion protection
 		Chat removedChat = chats.remove(chat.getName());
 		if (removedChat == null) {
 			throw new IllegalArgumentException("Trying to remove an unknown chat with name \'"
 					+ chat.getName() + "\'");
 		}
 
+		//TODO should it be included in a mutual exclusion zone?
 		for(User user : users.values()){
 			user.chatClosed(removedChat);
 		}
 	}
 
 	public Collection<Chat> getChats() {
+		//TODO should it be included in a mutual exclusion zone?
 		return Collections.unmodifiableCollection(chats.values());
 	}
 
 	public Chat getChat(String chatName) {
+		//TODO add mutual exclusion protection
 		return chats.get(chatName);
 	}
 
 	public Collection<User> getUsers() {
+		//TODO should it be included in a mutual exclusion zone?
 		return Collections.unmodifiableCollection(users.values());
 	}
 
 	public User getUser(String userName) {
+		//TODO add mutual exclusion protection
 		return users.get(userName);
 	}
 
