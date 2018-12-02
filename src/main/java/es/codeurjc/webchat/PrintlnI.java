33f665eff452e85aa9bae2d2980f005b65be0b52
jramed <jramed@gmail.com>
2018-12-02 18:55:37 +0100

    Add indented print to see more clear the traces
    In multithread environment, when printing the output it is a mess.
    An option is to print with some identation the output. In this case
    also the thread name is printed.
    This version is tightly coupled to the name of the thread generated by
    Executor.

---
+++ src/main/java/es/codeurjc/webchat/PrintlnI.java
@@ -0,0 +1,45 @@
+package es.codeurjc.webchat;
+
+import java.util.ArrayList;
+
+public class PrintlnI {
+	private static final ArrayList<String> spaces = new ArrayList<String>();
+	
+	public static void initPerThread() 
+	{
+		if (spaces.size() == 0) {
+			spaces.add("");
+		} else {
+			spaces.add(spaces.get(spaces.size() - 1) + "        ");
+		}
+	}
+
+	public static void printlnI(String text, String threadName)
+	{
+		if (threadName.isEmpty()) {
+			threadName = Thread.currentThread().getName();
+		}
+		String parts[] = threadName.split("-");
+		int threadNum = 1;
+		if (parts.length >= 3)
+		{
+			threadNum = Integer.parseInt(parts[3]);
+		} 
+		sleepRandom(10);
+		System.out.println(spaces.get(threadNum-1) + threadName + ": " + text);
+		sleepRandom(10);
+	}
+
+	public static void sleepRandom(long millis) {
+		sleep((long) (Math.random() * millis));
+	}
+
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
