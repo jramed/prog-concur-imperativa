7ecdd703550b0d4cdeb4206fda160368d17d7fbf
jramed <jramed@gmail.com>
2018-12-07 21:37:11 +0100

    handle a sendmessage in a thread per user
    A completionSErvice is used with and executor of one
    single thread to manage the request send for a given
    user. The completionService is created when the user
    is added to a chat and shutdown when the user is
    removed from the chat.
    
    The test sometimes fail because the order is not the
    correct one.
    Still pending to change the other notifications, when
    a user is added to the chat or a user leave a chat.

--- src/main/java/es/codeurjc/webchat/Chat.java
+++ src/main/java/es/codeurjc/webchat/Chat.java
@@ -2,20 +2,45 @@ package es.codeurjc.webchat;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Chat {
+	
+	private class CustomPair {
+	    private ExecutorService executor;
+	    private CompletionService<String> completionServices;
+	    
+		public CustomPair(ExecutorService executor2, CompletionService<String> completionServices) {
+			super();
+			this.executor = executor2;
+			this.completionServices = completionServices;
+		}
+		public ExecutorService getExecutor() {
+			return executor;
+		}
+
+		public CompletionService<String> getCompletionServices() {
+			return completionServices;
+		}
+    
+	}
 
 	private String name;
 	private ChatManager chatManager;
 
 	private ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+	private ConcurrentMap<String, CustomPair> taskPerUser = new ConcurrentHashMap<>();
 
 
 	public Chat(ChatManager chatManager, String name) {
 		this.chatManager = chatManager;
-		this.name = name;
+		this.name = name;		
 	}
 
 	public String getName() {
@@ -26,6 +51,11 @@ public class Chat {
 		User previousUser = users.putIfAbsent(user.getName(), user);
 		if (previousUser != null) {
 			PrintlnI.printlnI("There was a previous user with the name: " + user.getName(), "");
+		} else {
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			CompletionService<String> completionService = new ExecutorCompletionService<>(executor);
+			CustomPair pair = new CustomPair(executor,completionService);
+			taskPerUser.putIfAbsent(user.getName(), pair);
 		}
 		synchronized (users) {
 			for(User u : users.values()){
@@ -36,13 +66,20 @@ public class Chat {
 		}
 	}
 
-	public void removeUser(User user) {
+	public void removeUser(User user) throws InterruptedException {
 		try {
 			users.remove(user.getName(), user);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
 		}
+		
+		CustomPair pair = taskPerUser.get(user.getName());
+		ExecutorService executor = pair.getExecutor();
+		executor.shutdown();
+		executor.awaitTermination(10, TimeUnit.SECONDS);
+		taskPerUser.remove(user.getName(), pair);
+
 		for(User u : users.values()){
 			u.userExitedFromChat(this, user);
 		}
@@ -59,12 +96,17 @@ public class Chat {
 	public void sendMessage(User user, String message) {
 		synchronized (users) {
 			for(User u : users.values()){
-				u.newMessage(this, user, message);
+				taskPerUser.get(user.getName()).getCompletionServices().submit(()->sendMessageToUser(u, message));
 			}
 		}
 	}
 
 	public void close() {
 		this.chatManager.closeChat(this);
+	}
+	
+	private String sendMessageToUser(User user, String message) {	
+		user.newMessage(this, user, message);
+		return user.getName()+message;
 	}
 }
